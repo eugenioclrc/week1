b75ffc2b00222a865a876af8a9149e7747d55fd1
eugenioclrc <eugenioclrc@gmail.com>
2022-05-09 01:18:23 -0300

    week 1 submission

--- Q2/scripts/bump-solidity.js
+++ Q2/scripts/bump-solidity.js
@@ -1,12 +1,24 @@
 const fs = require("fs");
-const solidityRegex = /pragma solidity \^\d+\.\d+\.\d+/
+let solidityRegex = /pragma solidity \^\d+\.\d+\.\d+/
 
-const verifierRegex = /contract Verifier/
+let verifierRegex = /contract Verifier/
 
 let content = fs.readFileSync("./contracts/HelloWorldVerifier.sol", { encoding: 'utf-8' });
 let bumped = content.replace(solidityRegex, 'pragma solidity ^0.8.0');
 bumped = bumped.replace(verifierRegex, 'contract HelloWorldVerifier');
 
 fs.writeFileSync("./contracts/HelloWorldVerifier.sol", bumped);
+
+// [assignment] add your own scripts below to modify the other verifier contracts you will build during the assignment
+
+solidityRegex = /pragma solidity \^\d+\.\d+\.\d+/
+
+verifierRegex = /contract Verifier/
+
+content = fs.readFileSync("./contracts/Multiplier3Groth16Verifier.sol", { encoding: 'utf-8' });
+bumped = content.replace(solidityRegex, 'pragma solidity ^0.8.0');
+bumped = bumped.replace(verifierRegex, 'contract Multiplier3');
+
+fs.writeFileSync("./contracts/Multiplier3Groth16Verifier.sol", bumped);
 
 // [assignment] add your own scripts below to modify the other verifier contracts you will build during the assignment
